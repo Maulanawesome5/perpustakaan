2077d002a3f62448dd5b406ca86638961b2d3d7c
maulanawesome5 <maulanawesome5@gmail.com>
2023-10-23 02:50:53 +0700

    Membuat function detail produk buku

--- produk/views.py
+++ produk/views.py
@@ -10,3 +10,12 @@ def index(request):
         'website': 'Perpustakaan'
     }
     return render(request, 'produk/index.html', context)
+
+def detail(request, inputSlug):
+    buku = Buku.objects.get(slug=inputSlug)
+    context = {
+        'books': buku,
+        'halaman': 'Detail Buku',
+        'website': 'Perpustakaan'
+    }
+    return render(request, 'produk/detail.html', context)
