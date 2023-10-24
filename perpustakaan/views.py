092f403976645f826116348de6229464b318161e
maulanawesome5 <maulanawesome5@gmail.com>
2023-10-24 16:19:09 +0700

    import data buku dari app produk, display ke home

--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from produk.models import Buku as data_buku
+
 
 def index(request):
+    buku = data_buku.objects.all()
     context = {
+        'books':buku,
         'halaman':'Beranda',
         'website':'Perpustakaan'
     }
