8bbc8f6d4d0dc83e9d8acfaaafe27b69d8607330
maulanawesome5 <maulanawesome5@gmail.com>
2024-02-15 18:34:07 +0700

    Migrasi ke DBMS PostgreSQL

--- perpustakaan/settings.py
+++ perpustakaan/settings.py
@@ -77,11 +77,29 @@ WSGI_APPLICATION = 'perpustakaan.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'toko_buku',
+        'USER': 'postgres',
+        'PASSWORD': 'Persebaya1927',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
+# Database Router
+# Dengan cara ini, Anda dapat menggunakan dua DBMS berbeda dalam satu proyek Django.
+# Pastikan untuk mengganti konfigurasi sesuai dengan kebutuhan spesifik Anda.
+# DATABASE_ROUTERS = ['produk.routers.MyRouter']
+
+# Serialization timezone format
+# SERIALIZATION_MODULES = {
+#     'custom_json': 'produk.serializers.CustomJSONEncoder',
+# }
 
 
 # Password validation
@@ -112,7 +130,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
