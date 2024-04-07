7657b789cf72361e3635756b5b4a93c20dc7e668
maulanawesome5 <maulanawesome5@gmail.com>
2024-04-08 05:48:19 +0700

    Membuat models Provinsi dan Kota/Kabupaten

--- wilayah/admin.py
+++ wilayah/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Provinsi, Kabupaten_Kota
 
 # Register your models here.
+admin.site.register(Provinsi)
+admin.site.register(Kabupaten_Kota)
--- wilayah/models.py
+++ wilayah/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Provinsi(models.Model):
+    provinsi = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id}. {self.provinsi}"
+
+    class Meta:
+        ordering = ["id"]
+
+
+class Kabupaten_Kota(models.Model):
+    provinsi = models.ForeignKey(Provinsi, on_delete=models.CASCADE)
+    kabupaten_kota = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.id}. {self.kabupaten_kota}"
+
+    class Meta:
+        ordering = ["id"]
