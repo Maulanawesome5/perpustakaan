0e4c44bab0b267c537d5008a7b698314c3fb9ce2
maulanawesome5 <maulanawesome5@gmail.com>
2024-04-16 23:45:44 +0700

    update tabel class Kabupaten_Kota

--- wilayah/models.py
+++ wilayah/models.py
@@ -13,11 +13,21 @@ class Provinsi(models.Model):
 
 
 class Kabupaten_Kota(models.Model):
+    PREDIKAT_CHOICES = (("Kabupaten", "Kabupaten"), ("Kota", "Kota"),
+                        ("DKI", "Daerah Khusus Ibukota"), ("DI", "Daerah Istimewa"),
+                        ("Kota Adm.", "Kota Administrasi"), ("Kab. Adm.", "Kabupaten Administrasi"))
     provinsi = models.ForeignKey(Provinsi, on_delete=models.CASCADE)
+    predikat = models.CharField(max_length=20, blank=True, null=True,
+                                choices=PREDIKAT_CHOICES)
     kabupaten_kota = models.CharField(max_length=100)
 
     def __str__(self) -> str:
-        return f"{self.id}. {self.kabupaten_kota}"
+        return f"{self.predikat} {self.kabupaten_kota}"
 
     class Meta:
-        ordering = ["id"]
+        ordering = ["provinsi_id", "id"]
+
+
+class Distrik(models.Model):
+    # distrik = models.CharField(max_length=50)
+    pass
