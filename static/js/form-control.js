64da39e381a0d4ba8568b9ab1064514c8deca9e5
maulanawesome5 <maulanawesome5@gmail.com>
2024-05-15 15:36:23 +0700

    Membuat custom javascript untuk desain tampilan form group bootstrap

---
+++ static/js/form-control.js
@@ -0,0 +1,10 @@
+// 1. Mengubah tampilan kolom password dengan gaya form-control menggunakan Bootstrap
+let col_password1 = document.getElementById("id_password1");
+let col_password2 = document.getElementById("id_password2");
+let col_username = document.getElementById("id_username");
+let col_password = document.getElementById("id_password");
+
+$(col_password1).addClass("form-control");
+$(col_password2).addClass("form-control");
+$(col_username).addClass("form-control");
+$(col_password).addClass("form-control");
